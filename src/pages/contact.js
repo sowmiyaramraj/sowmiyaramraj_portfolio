a766ba96149e2c7d5e95ee9abdb75294032d3b5a
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-16 18:48:37 +0530

    first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Contact()
+{
+    return(
+        <div>
+            <h1>contacty</h1>
+        </div>
+    );
+
+}
---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Contact()
+{
+    return(
+        <div>
+            <h1>home</h1>
+        </div>
+    );
+
+}
