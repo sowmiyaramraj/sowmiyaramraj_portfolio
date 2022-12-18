8fba9398fca57cbaa0d519000c3260dca9594f06
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-18 18:00:50 +0530

    -

--- src/pages/education.js
+++ src/pages/education.js
@@ -1,9 +1,65 @@
 import React from "react";
+import msu from "../images/msu.jpeg";
+import sarada from "../images/sarada.png";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
 export default function Education()
 {
     return(
         <div>
-            <h1>Education</h1>
+            <div >
+               
+                <Grid container spacing={0}>
+                    <Grid item xs={3}>
+                    <img style={{width:"150px",height:"100px"}} src={msu}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <Card sx={{ minWidth: 275 }}>
+                    <Typography sx={{ fontSize: 14,textAlign:"center",background:"rgb(204, 245, 245)" }} color="text.secondary" gutterBottom>
+        MS University<br/>
+        Master of Science in Infromation Technology(2016-2018)
+        </Typography>
+        <Typography>
+            <ul style={{textAlign:"left"}}>
+               
+                I have studied basic Big Data analytics<br/>
+               
+               Basic coding in R studio
+            </ul>
+            
+            </Typography>  </Card>
+                    </Grid>
+                </Grid>
+            </div>
+            <br/>
+            <div >
+               
+                <Grid container spacing={0}>
+                    <Grid item xs={3}>
+                    <img style={{width:"150px",height:"100px"}} src={sarada}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <Card sx={{ minWidth: 275 }}>
+                    <Typography sx={{ fontSize: 14,textAlign:"center",background:"rgb(204, 245, 245)" }} color="text.secondary" gutterBottom>
+        Sri Sarada college<br/>
+       Bachelore of Science in Computer Science(2013-2016)
+        </Typography>
+        <Typography>
+            <ul style={{textAlign:"left"}}>               
+                I have studied basic Big Data analytics<br/>               
+               Basic coding in R studio
+            </ul>
+            
+            </Typography>  </Card>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     );
 
