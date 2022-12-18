591d933e6aeb5b3ec6d410194e34435e9d3b181a
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-18 13:17:10 +0530

    -

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Typography from '@mui/material/Typography';
+import github from "../images/githubicon.png";
+import email from "../images/emaillogo.png";
+import insta from "../images/instalogo.jpeg";
+import linkedin from "../images/linkedinlogo.png";
+import cvlogo from "../images/cvlogo.png";
 export default function Contact()
 {
     return(
         <div>
           <Typography>Reach Out to me! </Typography>
-          <i class="fa fa-instagram fa-3x"></i>
-          <FontAwesomeIcon icon="fa-brands fa-instagram" />
+          
+          <a href="https://github.com/sowmiyaramraj" target="_blank" rel="noreferrer">
+          <img style={{width:"35px",height:"35px"}} src={github} alt="github" />
+        </a>&nbsp;
+        <a href="https://github.com/sowmiyaramraj" target="_blank" rel="noreferrer">
+          <img style={{width:"35px",height:"35px"}} src={email} alt="github" />
+        </a>&nbsp;
+        <a href="www.linkedin.com/in/sowmiya-ramraj" target="_blank" rel="noreferrer">
+          <img style={{width:"35px",height:"35px"}} src={linkedin} alt="github" />
+        </a>&nbsp;
+        <a href="https://www.instagram.com/yoon_kids_mom" target="_blank" rel="noreferrer">
+          <img style={{width:"35px",height:"35px"}} src={insta} alt="github" />
+        </a>&nbsp;
+        <br/><br/>
+        <a href="https://github.com/sowmiyaramraj/sowmiyaRamraj-Resume" target="_blank" rel="noreferrer">
+          <img style={{width:"55px",height:"55px"}} src={cvlogo} alt="github" />
+        </a>&nbsp;
+       
         </div>
     );
 
