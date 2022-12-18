5f6c1a77738890f7e1fda0046f84f0f314a80144
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-18 14:59:37 +0530

    -

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,12 +5,24 @@ import email from "../images/emaillogo.png";
 import insta from "../images/instalogo.jpeg";
 import linkedin from "../images/linkedinlogo.png";
 import cvlogo from "../images/cvlogo.png";
+import sowimg from "../images/sowmiyaimg.jpg";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import contactbg from "../images/contactbg.jpeg";
+
+
 export default function Contact()
 {
     return(
         <div>
-          <Typography>Reach Out to me! </Typography>
-          
+           <Box sx={{ flexGrow: 1 }}>
+           <Typography variant="h3" gutterBottom>Reach Out to me! </Typography>
+           <Typography style={{color:"grey"}} variant="h6" gutterBottom>DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.</Typography>
+      <Grid  style={{width:"maxWidth",height:"250px"}}>
+        <Grid item xs={6}>
+            <img style={{width:"250px",height:"250px"}} src={sowimg} alt="profile"/>
+        </Grid>
+        <Grid item xs={6}>
           <a href="https://github.com/sowmiyaramraj" target="_blank" rel="noreferrer">
           <img style={{width:"35px",height:"35px"}} src={github} alt="github" />
         </a>&nbsp;
@@ -27,7 +39,15 @@ export default function Contact()
         <a href="https://github.com/sowmiyaramraj/sowmiyaRamraj-Resume" target="_blank" rel="noreferrer">
           <img style={{width:"55px",height:"55px"}} src={cvlogo} alt="github" />
         </a>&nbsp;
-       
+
+        <Typography style={{fontWeight:"bolder"}} variant="h5" gutterBottom>Address</Typography>
+        <p style={{color:"grey",fontFamily:"cursive",fontStyle:"oblique",fontWeight:"bolder"}}>No.5, Alagar salai<br/>Old Pallavarm,Chennai-600117</p>
+        <Typography style={{fontWeight:"bolder"}} variant="h5" gutterBottom>Phone number</Typography>
+        <p style={{color:"grey",fontFamily:"cursive",fontStyle:"oblique",fontWeight:"bolder"}}>8122802274</p>
+        </Grid>
+        </Grid>
+    </Box>
+         
         </div>
     );
 
